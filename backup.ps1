d7da4fc3c12d68936e205a5709d2a0a40d2f47fe
q4p14 <a-schneider9@web.de>
2017-09-06 11:34:44 +0200

    blub

--- backup.ps1
+++ backup.ps1
@@ -6,45 +6,59 @@ $global:src = "C:\Users\bob\Documents\Test"
 $global:dst = "C:\Users\bob\Documents\Backups"
 # Get the current date for renaming folders later
 $current_date = Get-Date -Format "yyyy-MM-dd-hh-mm"
+$check_date = Get-Date
 # Set the time offsets to decide when to do a full, differential and incremental
 # backup
 $offset_full = New-TimeSpan -Hours 1
 $timespan_diff = New-TimeSpan -Minutes 10
 # Pattern matching created folders
-$bak_pattern = '^................-[v,i,d]$'
-#$src_itmes = Get-ChildItem -Path "$global:src" -Recursive
+$bak_pattern = '^....-..-..-..-..-[v,i,d]$'
+#$src_itmes = Get-ChildItem -Path "$global:src" -Recurse
+# Get the latest backup in the backup base directory
+$dst_last = Get-ChildItem -Directory -Path $global:dst | Sort-Object `
+LastWriteTime -Descending | Select-Object -First 1
 
 # Full Backup Function
-function FullBackup ($local:src,$local:dst){
+function FullBackup (){
+  $local:src = $global:src
+  $local:dst = $global:dst
 
+  Copy-Item -Recurse -Path "$local:src" -Destination `
+  "$local:dst\$current_date-v"
 }
 
 # Differential Backup Function
-function DiffBackup ($local:src,$local:dst){
-
+function DiffBackup (){
+  $local:src = $global:src
+  $local:dst = $global:dst
 }
 
 # Incremental Backup Function
-function IncrBackup ($local:src,$local:dst){
-
+function IncrBackup (){
+  $local:src = $global:src
+  $local:dst = $global:dst
 }
 
 # If there is no previous backup, start a full backup. Else...
-If (!Get-ChildItem -Path "$global:dst" -Filter "$bak_pattern")
+If (!(Get-ChildItem -Path "$global:dst" | Where-Object { $_.Name -match `
+  $bak_pattern }))
 {
   FullBackup $gloabal:dst $gloabal:src
+  $test = Get-ChildItem -Path "$global:dst" -Filter "$bak_pattern"
+  Write-Host $test
 }else{
   # ... decide which backup to run:
-  # If the files are older than the offset for a full backup, start a full backup.
+  # If the files are older than the offset for a full backup, start a full
+  # backup.
   # If the files are older than the offset for the differential backup, run a
   # differential backup.
   # Otherwise the file is newer than the offset for the diff backup and an
   # incremental backup will be run.
-  If (Test-Path -Path $dst_fin -OlderThan ($current_date - $timespan_full))
+  If (Test-Path -Path $dst_last -OlderThan ($check_date - $timespan_full))
   {
     FullBackup $gloabal:dst $gloabal:src
   }else{
-    If (Test-Path -Path $dst_fin -OlderThan ($current_date - $timespan_diff))
+    If (Test-Path -Path $dst_last -OlderThan ($check_date - $timespan_diff))
     {
       DiffBackup $gloabal:dst $gloabal:src
     }else{
